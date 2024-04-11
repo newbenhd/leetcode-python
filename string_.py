4152d55acdbd615082f5bf889402d85ae14bc0ff
newbenhd <newben.hd@gmail.com>
2024-04-11 14:34:13 -0500

    LC 424. Longest Repeating Character Replacement

--- string_.py
+++ string_.py
@@ -1,4 +1,35 @@
 class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # sum(minor_character_count) < k.
+        # major = A
+        # {
+        #   A: 4,
+        #   B: 2,
+        #   C: 3,
+        # }
+        # L: 0
+        # The overall plan is that when we count the substring
+        # we have to keep the sum of minor characters, meaning
+        # any characters which is not major, and constantly update
+        # the current major character. if the sum of minor character
+        # is greater than k, then we increment the left pointer until
+        # the sum is less than K (also, update the major character).
+        # During iteration, keep track of the maximum substring length.
+        # At the end of iteration, we return the maximum substring length.
+        L = 0
+        dominant = 0
+        highest = 0
+        cache = [0] * 26
+        index = lambda x: ord(x) - ord("A")
+        for i in range(len(s)):
+            cache[index(s[i])] += 1
+            dominant = max(dominant, cache[index(s[i])])
+            if i - L + 1 > k + dominant:
+                cache[index(s[L])] -= 1
+                L += 1
+            highest = max(highest, i - L + 1)
+        return highest
+
     def lengthOfLongestSubstring(self, s: str) -> int:
         # keep track of substring without repeating characters using two pointers: left and right
         # iterate through the input string and check if the current substring contains duplicate
