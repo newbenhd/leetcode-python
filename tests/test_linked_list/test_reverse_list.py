19f33fb18b0d150b3bc9823b401c58c2b12f9a60
newbenhd <newben.hd@gmail.com>
2024-04-21 13:38:42 -0500

    LC 206 Reverse Linked List

---
+++ tests/test_linked_list/test_reverse_list.py
@@ -0,0 +1,25 @@
+import pytest
+from linked_list import ListNode
+
+
+@pytest.mark.parametrize(
+    "head, output",
+    [
+        (
+            ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))),
+            ListNode(5, ListNode(4, ListNode(3, ListNode(2, ListNode(1))))),
+        ),
+        (ListNode(1, ListNode(2)), ListNode(2, ListNode(1))),
+        (None, None),
+    ],
+)
+def test_reverse_list(solution, head, output):
+    assert solution.reverseList(head) == output
+
+
+@pytest.mark.parametrize(
+    "head, other",
+    [(ListNode(1), None), (ListNode(1, ListNode(2)), ListNode(2, ListNode(3)))],
+)
+def test_list_node_equal(head, other):
+    assert head != other
