d22c08594be935f9c492f404622667740e27095e
newbenhd <newben.hd@gmail.com>
2024-05-03 11:33:12 -0500

    LC 57. Insert Interval

---
+++ interval.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def insert(
+        self, intervals: List[List[int]], newInterval: List[int]
+    ) -> List[List[int]]:
+        output = []
+        for i in range(len(intervals)):
+            if intervals[i][1] < newInterval[0]:
+                output.append(intervals[i])
+            elif intervals[i][0] > newInterval[1]:
+                output.append(newInterval)
+                return output + intervals[i:]
+            else:
+                newInterval = [
+                    min(intervals[i][0], newInterval[0]),
+                    max(intervals[i][1], newInterval[1]),
+                ]
+        output.append(newInterval)
+        return output
---
+++ tests/test_interval/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+from interval import Solution
+
+
+@pytest.fixture()
+def solution():
+    return Solution()
