71b38f18316778fbbe741e6ad6d2b5fe7f7494e5
newbenhd <newben.hd@gmail.com>
2024-05-07 13:24:05 -0500

    LC 380 Insert Delete GetRandom O(1)

--- top150/array_.py
+++ top150/array_.py
@@ -1,4 +1,36 @@
 from typing import List
+import random
+
+
+class RandomizedSet:
+    def __init__(self):
+        self.position = []
+        self.index = {}
+        # same propability of randomly pick element
+        # O(1) for every function
+
+    def search(self, val: int) -> bool:
+        return val in self.index
+
+    def insert(self, val: int) -> bool:
+        if self.search(val):
+            return False
+        self.position.append(val)
+        self.index[val] = len(self.position) - 1
+        return True
+
+    def remove(self, val: int) -> bool:
+        if not self.search(val):
+            return False
+        idx = self.index[val]
+        self.position[idx] = self.position[-1]
+        self.index[self.position[-1]] = idx
+        self.position.pop()
+        del self.index[val]
+        return True
+
+    def getRandom(self) -> int:
+        return random.choice(self.position)
 
 
 class Solution:
